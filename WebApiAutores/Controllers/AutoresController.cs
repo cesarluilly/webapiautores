f3f6b5a38895fb8a84000cdc921c933a6d57852e
cesarluilly <cesarluilly.garcia@hotmail.com>
2022-08-06 02:07:19 -0500

    Actualizando y Borrando Recursos

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -30,5 +30,47 @@ namespace WebApiAutores.Controllers
             await _context.SaveChangesAsync();
             return Ok();
         }
+
+        [HttpPut("{id:int}")] // api/autores
+        public async Task<ActionResult> put(Autor autor, int id)
+        {
+            if (
+                autor.Id != id
+                )
+            {
+                return BadRequest("El id del autor no coincide con el ID de la URL");
+            }
+
+            var existe = await _context.Autores.AnyAsync(x => x.Id == id);
+
+            if (
+                !existe
+                )
+            {
+                return NotFound();
+            }
+
+            _context.Update(autor);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] //api/autores/2
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await _context.Autores.AnyAsync(x => x.Id == id);
+
+            if(
+                !existe
+                )
+            {
+                return NotFound();
+            }
+
+            _context.Remove(new Autor() { Id = id});
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
